92d6ffeddef9afe7785d82e7443018a65cf922b1
nadia-rais <nadia.rais@laplateforme.io>
2020-05-18 18:08:11 +0200

    header
    1er essai header

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+
+<header>
+<div id="logohead">
+    <a href="index.php"><h5 id="toptitle">MUSEUM OF MARSEILLE</h5></a>
+    </div>
+    <div>
+        <?php 
+          if (isset($_SESSION["login"])) {
+        ?>
+        <a href="profil.php"></a> 
+        <a href="commentaire.php"></a> 
+        <a href="livre-or.php"></a>
+        <a href="index.php?deco">Déconnexion</a>
+        <?php
+        }else{
+        ?>
+        <a href="connexion.php"><button class="log">Login</button></a> 
+        <a href="inscription.php"><button class="reg">Sign up</button></a> 
+        <?php
+        }
+        ?>
+</header>
