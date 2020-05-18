111e62dc0f55b3353670d7290aec426a89d2c516
lea-martel <lea.martel@laplateforme.io>
2020-05-18 18:16:08 +0200

    formulaire inscription

--- inscription.php
+++ inscription.php
@@ -1,19 +1,53 @@
-<?php session_start(); ?>
+<?php session_start();
+
+$bdd =  mysqli_connect("localhost", "root", "", "livreor");
+
+
+if (isset($_POST))
+
+
+?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>musée guestbook - inscription</title>
+    <title>Musée Guestbook - Inscription</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=yes"/>
+    <meta name="viewport" content="width=device-width, user-scalable=yes" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
-<body>
-    <?php include("includes/header.php");?>
-    <main>
+<header>
+    <?php include("includes/header.php"); ?>
+</header>
 
+<body>
+    <main>
+    <?php
+    
+    if (isset($_SESSION["login"]))
+    echo "Vous êtes déjà inscrit";
+
+    ?>
+        <form action="inscription.php" method="post">
+            <h1 class="toptitre"> INSCRIPTION </h1> <br />
+            <label class="champs">Login</label>
+            <input class="cadre" type="texte" id="login" name="login" placeholder="Entrer votre login"> <br /> <br />
+
+            <label class="champs">Mot de passe</label>
+            <input class="cadre" type="password" id="mdp" name="password" placeholder="Entrer le mot de passe"> <br /> <br />
+
+            <label class="champs">Confirmation mot de passe</label>
+            <input class="cadre" type="password" id="mdp" name="password" placeholder="Confirmer le mot de passe"> <br /> <br />
+
+            <input class="button" type="submit" value="CONNEXION" name="connexion"> <br /> <br />
+            <p class="connecte">Vous avez déjà un compte ? <a href="connexion.php">Connectez-vous.</a></p>
+        </form>
     </main>
-    <?php include("includes/footer.php"); ?>
+    <footer>
+        <?php include("includes/footer.php"); ?>
+    </footer>
 </body>
+
 </html>
