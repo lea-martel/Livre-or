91da2fa7ed51ee2ab90dda9f9d2fe3f16dd5180c
nadia-rais <nadia.rais@laplateforme.io>
2020-05-18 15:09:58 +0200

    création des fichiers
    creation des fichiers de travail

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>musée guestbook - accueil</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=yes"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <?php include("includes/header.php") ?>
+    <main>
+
+    </main>
+    <?php include("includes/footer.php") ?>
+
+</body>
+</html>
