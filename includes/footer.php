69a09a2b10568adf1fc8b4d1409e4bb9991a48e6
nadia-rais <nadia.rais@laplateforme.io>
2020-05-18 19:41:51 +0200

    includes+ css
    modification des fichiers + correction style + responsive

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <div id="footer-container">
+      <div id="left-col">
+        <img src="https://i.ibb.co/BfRDpzm/logo-museumofmarseille.png" alt="logo-logo-museumofmarseille" class="logo">
+        <p id="rights-text">© 2020 created by museum of marseille</p>
+      </div>
+      <div id="social-media1">
+        <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+      </div>
+      <div id="right-col">
+        <h6 id="news">Newsletter</h6>
+        <div id="border"></div>
+        <form action="" id="newsletter-form">
+          <input type="text" class="txtb" placeholder="Enter your email">
+          <input type="submit" class="btn" value="submit">
+        </form>
+      </div>
+    </div>
+</footer>
--- includes/header.php
+++ includes/header.php
@@ -1,22 +1,23 @@
 
 <header>
-<div id="logohead">
+  <div id="logohead">
     <a href="index.php"><h5 id="toptitle">MUSEUM OF MARSEILLE</h5></a>
-    </div>
-    <div>
-        <?php 
-          if (isset($_SESSION["login"])) {
-        ?>
-        <a href="profil.php"></a> 
-        <a href="commentaire.php"></a> 
-        <a href="livre-or.php"></a>
-        <a href="index.php?deco">Déconnexion</a>
-        <?php
-        }else{
-        ?>
-        <a href="connexion.php"><button class="log">Login</button></a> 
-        <a href="inscription.php"><button class="reg">Sign up</button></a> 
-        <?php
-        }
-        ?>
+  </div>
+  <div id="connect-but">
+     <?php 
+       if (isset($_SESSION["login"])) {
+     ?>
+      <a href="profil.php"></a> 
+      <a href="commentaire.php"></a> 
+      <a href="livre-or.php"></a>
+      <a href="index.php?deco">Déconnexion</a>
+      <?php
+      }else{
+      ?>
+      <a href="connexion.php"><button class="log">Login</button></a> 
+      <a href="inscription.php"><button class="reg">Sign up</button></a> 
+      <?php
+      }
+      ?>
+  </div>
 </header>
