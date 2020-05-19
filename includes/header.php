cb7926841170d9e0b670927df06c21f432b3c6a9
nadia-rais <nadia.rais@laplateforme.io>
2020-05-19 19:52:58 +0200

    index + connexion + css
    fichier index qui s'adapte lorsqu'on se connecte + connexion php + correction header + css

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,25 @@
 <header>
     <nav>
-    <li><a href="index.php"><h5 id="toptitle">WELCOME TO MUMA !</h5></a></li>
-    <?php 
-       if (isset($_SESSION["login"])){
-        echo "<nav><ul><li><a href='profil.php'></a></li>";
-        echo "<li><a href='commentaire.php'></a></li>"; 
-        echo "<li><a href='livre-or.php'></a></li>";
-        echo "<li><a href='index.php?deco'>Déconnexion</a></li></nav>";
+      <a href="index.php"><h5 id="toptitle">WELCOME TO MUMA !</h5></a>
+      <?php 
+        if (isset($_SESSION["login"])){
+          echo '<nav class="main-navigation">
+                  <div class="nav-wrapper">
+                    <input id="menu-checkbox" type="checkbox" class="menu-checkbox"/>
+                    <label for="menu-checkbox" class="menu-toggle">&equiv; MENU</label>
+                    <ul class="menu">
+                      <li><a href="livre-or.php">GUESTBOOK</a></li>
+                      <li><a href="commentaire.php">LAISSER UN COMMENTAIRE</a></li>
+                      <li><a href="profil.php">PROFIL</a></li>
+                    </ul> 
+                  </div> 
+                </nav>'; 
+        }
+       else{
+         echo '<div id="connect-but"><a href="connexion.php"><button class="log">Login</button></a>
+               <a href="inscription.php"><button class="reg">Sign up</button></a></div>';
        }
     ?>
-    <div id="connect-but">
-      <a href="connexion.php"><button class="log">Login</button></a></li> 
-      <a href="inscription.php"><button class="reg">Sign up</button></a></li> 
-    </div>
    </nav>
   </div>
 </header>
