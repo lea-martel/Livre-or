9ece576d37b7d1ac378f03489947aee12def21e2
nadia-rais <nadia.rais@laplateforme.io>
2020-05-19 08:44:14 +0200

    index ajout condition
    condition ajoutée pour modifier la page d'accueil en se connectant à tester

--- index.php
+++ index.php
@@ -20,9 +20,26 @@
           <h1 id="title-index">MUSEUM OF MARSEILLE</h1>
         </div>
         <div id="presentation-index">
-          <p id="text-index">Vous venez de visiter notre musée... rejoignez notre communauté et partagez votre avis sur votre expérience ou une exposition !</p>
-          <a href="connexion.php"><h2 id="link-index">VOS COMMENTAIRES</h2></a>
-          <a id="link2-index" href="inscription.php">Pas encore inscrit ? Cliquez ici</a>
+          <?php
+            if(isset($_SESSION['login'])){
+              echo "<h2><Bonjour ".$_SESSION["login"]."</h2>";
+              echo "<p>Vous êtes actuellement connecté. Accédez à votre <a href=\"profil.php\">profil</a></p>";
+              echo "<a href='commentaire.php'><h2 id='link-index'>LES COMMENTAIRES</h2></a>";
+              echo "<form action='index.php' method='post'>
+                      <input class='button-deco'  name='deco' value='Déconnexion' type='submit'/>
+                    </form>";
+            }
+            else{ 
+              echo "<p id='text-index'>Vous venez de visiter notre musée... rejoignez notre communauté et partagez votre avis sur votre expérience ou une exposition !</p>";
+              echo "<a href='connexion.php'><h2 id='link-index'>VOS COMMENTAIRES</h2></a>";
+              echo "<a href='inscription.php' id='link2-index'>Pas encore inscrit ? Cliquez ici</a>";
+            }
+             /* Version sans php
+             <p id="text-index">Vous venez de visiter notre musée... rejoignez notre communauté et partagez votre avis sur votre expérience ou une exposition !</p>
+             <a href="connexion.php"><h2 id="link-index">VOS COMMENTAIRES</h2></a>
+             <a id="link2-index" href="inscription.php">Pas encore inscrit ? Cliquez ici</a>
+             */
+          ?>       
         </div>
       </div>
      
