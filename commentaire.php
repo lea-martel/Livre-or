b77bd850ad2fb583fba52ebb1fe6768618e44c8f
nadia-rais <nadia.rais@laplateforme.io>
2020-05-20 15:52:56 +0200

    page commentaire.php
    page pour ajouter un commentaire quand on est connecté + css

--- commentaire.php
+++ commentaire.php
@@ -1,4 +1,28 @@
-<?php session_start(); ?>
+<?php session_start();
+
+$connect = mysqli_connect("localhost", "root", "", "livreor");
+
+if (isset($_POST['submit'])){
+
+    if (!empty($_POST['commentaire'] && strlen($_POST['commentaire']) > 15 )){
+
+        $request = "SELECT id FROM utilisateurs WHERE login ='".$_SESSION['login']."'";
+        $query = mysqli_query($connect, $request);
+        $id = mysqli_fetch_all($query);
+    
+        $com = $_POST['commentaire'];
+        $comment = addslashes($com);
+        $request2 = "INSERT INTO commentaires (commentaire, id_utilisateur, date) VALUES ('$comment', ".$id[0][0].", NOW())";
+        $query2 = mysqli_query($connect, $request2);
+    
+        mysqli_close($connect);
+    }  
+       
+    elseif(strlen ($_POST['commentaire']) < 15) {
+        $c_error="votre commentaire doit comporter 15 caractères au minimum...";
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -10,10 +34,32 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <?php include("includes/header.php") ?>
+    <header>
+      <?php include("includes/header.php") ?>
+    </header>
     <main>
-
+      <div id="container-com">
+        <h6 id="subtitle-com"> FAITES NOUS PART DE VOTRE EXPÉRIENCE 
+          <?php
+             echo '@'.$_SESSION['login'];
+          ?>
+        </h6>
+        <h1 id="title-com"> LAISSE-NOUS UN COMMENTAIRE </h1>
+        <div>
+          <form method="POST" action="commentaire.php" id="container-form">
+            <textarea id="textarea-com" type="textarea" name="commentaire" cols=70  rows=10 placeholder="votre expérience au museum of marseille..."></textarea><br/>
+            <input id= "button-com" type="submit" value="poster mon commentaire" name="submit" >
+          </form>
+          <?php
+            if(isset($c_error)){
+              echo '<div id="error-com">'.$c_error.'</div>';
+            }
+          ?>
+        </div>
+      </div>
     </main>
-    <?php include("includes/footer.php");?>
+    <footer>
+      <?php include("includes/footer.php");?>
+    </footer>
 </body>
 </html>
