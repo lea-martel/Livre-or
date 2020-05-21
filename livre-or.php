d2caea15badfe93560398577f161db20cce730f9
nadia-rais <nadia.rais@laplateforme.io>
2020-05-21 16:04:16 +0200

    brouillon livre or
    en cours...

--- livre-or.php
+++ livre-or.php
@@ -1,5 +1,23 @@
-<?php session_start(); ?>
+<?php session_start(); 
+$connect = mysqli_connect("localhost", "root", "root", 'livreor');
 
+$requete = "SELECT DATE_FORMAT (date, '%d/%m/%Y'),commentaire FROM commentaires ORDER by date DESC";
+$query = mysqli_query($connect , $requete);
+$dates = mysqli_fetch_all($query);
+
+$requete2 = "SELECT login FROM utilisateurs INNER JOIN commentaires ON utilisateurs.id = commentaires.id_utilisateur ORDER by date DESC";
+$query2 = mysqli_query($connect , $requete2);
+$login = mysqli_fetch_all($query2);
+
+$request = "SELECT DATE_FORMAT (date, '%d/%m/%Y'), utilisateurs.login, commentaires.commentaire FROM utilisateurs INNER JOIN commentaires ON utilisateurs.id = commentaires.id_utilisateur ORDER by date DESC";
+$query3 = mysqli_query($connect , $request);
+$test = mysqli_fetch_all($query3);
+
+
+mysqli_close($connect);
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,10 +28,53 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <?php include("includes/header.php");?>
+    <header>
+      <?php include("includes/header.php");?>
+    </header>
     <main>
-
+      <div id="title-comment">
+        <h1>les commentaires</h1>   
+      </div>
+      <div id="global">
+        <div id="boxtest">
+          <?php
+           foreach($dates as $key => $value){                 //boucle pour faire apparaître la date
+              echo '<p id="info-comment">posté le</p>'; 
+              foreach ($value as $key2=>$value2){ 
+                  echo '<p id="date">'.$value2. '</p><br/><br/>';
+              }
+            }
+          ?> 
+          <div id="container-com">
+            <?php
+              foreach($login as $key => $value){                    //boucle pour faire apparaître l'utilisateur
+                foreach ($value as $key2=>$value3){
+                   echo '<div id="user"><p id="info-comment">utilisateur</p>'.$value3. '</div>';
+                }
+              }
+            ?>
+        </div>
+      </div>
+      <table>
+        <thead></thead>
+          <?php
+            foreach($test as $key => $value){ 
+              echo '<tr><td>posté le,</td></tr>';
+              foreach ($value as $key2=>$value2){
+                echo  '</tr><td>'.$value2. '</td></tr></br>';
+            }
+          }
+        ?>
+      </table>
+    <?php
+     if (isset($_SESSION["login"])){
+          echo 'laissez-nous un commentaire @' .$_SESSION['login']; 
+        }
+      ?>
+    
     </main>
+    <footer>
     <?php include("includes/footer.php");?>
+    </footer>
 </body>
 </html>
